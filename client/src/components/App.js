c2551fdc1e1b5aef3919522b4ff0762c877f920c
WoodCollector <you@example.com>
2023-03-26 19:54:27 +0200

    frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 class App extends Component {
   state = { walletInfo: {} };
 
   componentDidMount() {
-    fetch("http://localhost:3000/api/wallet-info")
+    fetch(`${document.location.origin}/api/wallet-info`)
       .then((response) => response.json())
       .then((json) => this.setState({ walletInfo: json }));
   }
@@ -13,11 +15,25 @@ class App extends Component {
     const { address, balance } = this.state.walletInfo;
 
     return (
-      <div>
+      <div className="App">
+        <img className="logo" src={logo}></img>
+        <br />
         <div>Welcome to the blockchain!</div>
-
-        <div>Address: {address}</div>
-        <div>Balance: {balance}</div>
+        <br />
+        <div>
+          <Link to="/blocks">Blocks</Link>
+        </div>
+        <div>
+          <Link to="/conduct-transaction">Conduct a Transaction</Link>
+        </div>
+        <div>
+          <Link to="/transaction-pool">Transaction Pool</Link>
+        </div>
+        <br />
+        <div className="WalletInfo">
+          <div>Address: {address}</div>
+          <div>Balance: {balance}</div>
+        </div>
       </div>
     );
   }
